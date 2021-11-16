7130b2247e97092403a9469a9092b7ab534a9a7f
earamirezba <elkinramirezbarbosa@gmail.com>
2021-11-15 21:05:31 -0500

    navbar-Sidebar pages

---
+++ frontend/src/pages/Categorias.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Categorias () {
+  return (
+    <div className='usuarios'>
+      <h1>EDITAR CATEGORIAS</h1>
+    </div>
+  );
+}
+
+export default Categorias;
