e41795e1f8734af1a19713f424b6c325a7dcce6f
earamirezba <elkinramirezbarbosa@gmail.com>
2021-11-22 12:10:50 -0500

    commit ajustes sidebar

--- frontend/src/components/NavbarData.js
+++ frontend/src/components/NavbarData.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import * as FaIcons from 'react-icons/fa';
+
 
 export const NavbarData = [
 
@@ -17,10 +16,5 @@ export const NavbarData = [
     title: 'CONTACTENOS',
     path: '/categorias',
     cName: 'nav-text'
-  },
-  { 
-    icon: <FaIcons.FaCartPlus />,
-    path: '/products',
-    cName: 'nav-text'
-}
+  }
 ];
