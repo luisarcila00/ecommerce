c6aa5e064983d49a1492cac650bcf47819967e4e
smurillol <smurillo@utp.edu.co>
2021-11-30 18:55:26 -0500

    insertar imagen funcionando

--- frontend/src/components/modals/CategoryModal.js
+++ frontend/src/components/modals/CategoryModal.js
@@ -1,5 +1,8 @@
 import React, {useState} from "react";
 import {Button, Modal, Form, FloatingLabel} from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios';
+
 
 export const CategoryModal = ({modalTitle, categoryModalShow, setcategoryModalShow}) => {
   const [validated, setValidated] = useState(false);
@@ -15,6 +18,26 @@ export const CategoryModal = ({modalTitle, categoryModalShow, setcategoryModalSh
     setValidated(true);
   };
 console.log('categoryModalShow', categoryModalShow)
+const [imagen, setImagen] = useState(null);
+const subirImagen = e => {
+setImagen(e); 
+
+}
+const insertarImagen = async () => {
+  const f = new FormData();
+  for (let index = 0; index < imagen.length; index++) {
+    f.append("files", imagen[index]);	
+  }
+  await axios.post("http://localhost:3000/categorias", f)
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}
+
+
   return (
     <Modal
       size="lg"
@@ -30,10 +53,36 @@ console.log('categoryModalShow', categoryModalShow)
         </Modal.Header>
         <Modal.Body>
           <Form.Group id={'categorias'} className="mb-3">
-            <FloatingLabel controlId="floatingInputGrid" label="Nombre del cliente">
-              <Form.Control required type="text" placeholder="Nombre del cliente"/>
+            <FloatingLabel controlId="floatingInputGrid" label="Ingrese una categoría">
+              <Form.Control required type="text" placeholder="Ingrese una categoría"/>
               <Form.Control.Feedback type="invalid">
-                Debe ingresar un nombre.
+                Debe ingresar una categoría.
+              </Form.Control.Feedback>
+            </FloatingLabel>
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Body>
+          <Form.Group id={'imagenes'} className="mb-3">
+            <FloatingLabel controlId="floatingInputGrid">
+              <h3>Imagen</h3>
+              <input required type="file" name="files" multiple onChange={(e) => subirImagen(e.target.files)}/>
+                <br/><br/>
+                <Button className="btn btn-primary" onClick={() => insertarImagen}>
+                  Insertar imágenes
+                </Button>
+              <Form.Control.Feedback type="invalid">
+                Debe ingresar una imagen.
+                
+              </Form.Control.Feedback>
+            </FloatingLabel>
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Body>
+          <Form.Group id={'descripción'} className="mb-3">
+            <FloatingLabel controlId="floatingInputGrid" label="Descripción">
+              <Form.Control required type="text" placeholder="Descripción"/>
+              <Form.Control.Feedback type="invalid">
+                Debe ingresar una descripción.
               </Form.Control.Feedback>
             </FloatingLabel>
           </Form.Group>
