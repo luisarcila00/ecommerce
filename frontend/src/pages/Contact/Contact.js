955b6decf5d7310b6e1902be75addc37891dd833
smurillol <smurillo@utp.edu.co>
2021-12-03 16:03:37 -0500

    Página contact us terminada

--- frontend/src/pages/Contact/Contact.js
+++ frontend/src/pages/Contact/Contact.js
@@ -5,20 +5,19 @@ import { Form, Button} from "react-bootstrap";
 const Contact = () => {
   return (
     <div className="container">
-      <br/>
-      <h1 className="text-center">
-        Contáctanos
+      <h1 className="text-center" style={{ paddingTop: "10%" }}>
+        Contactános
       </h1>
-      <hr/>
-      <br/>
+      <hr />
+      <container>
       <Form>
   <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
     <Form.Label>Nombre completo</Form.Label>
-    <Form.Control type="text" placeholder="Ingresa tu nombre" />
+    <Form.Control type="text" placeholder="Ingresa tu nombre y apellido" />
   </Form.Group>
   <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
     <Form.Label>Teléfono</Form.Label>
-    <Form.Control type="number" placeholder="Ingresa tu numéro de teléfono" />
+    <Form.Control type="number" placeholder="Ingresa un número telefónico de contacto" />
   </Form.Group>
   <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
     <Form.Label>Correo electrónico</Form.Label>
@@ -26,11 +25,11 @@ const Contact = () => {
   </Form.Group>
   <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
     <Form.Label>Mensaje</Form.Label>
-    <Form.Control as="textarea" rows={3} placeholder="Escríbenos tus inquietudes"/>
+    <Form.Control as="textarea" rows={3} placeholder="Escribe tu mensaje en esta sección"/>
   </Form.Group>
   <Button variant="primary">Enviar</Button>
 </Form>
-
+      </container>
     </div>
   );
 };
